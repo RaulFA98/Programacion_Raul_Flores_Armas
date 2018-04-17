458708b1843953c8659adb622b3c6a7e3cbad378
RaulFA98 <36017529+RaulFA98@users.noreply.github.com>
2018-04-16 20:03:06 -0500

    Add files via upload

---
+++ Programacion_Raul/Test/television.java
@@ -0,0 +1,33 @@
+/*************************
+Raul Flores Armas
+Alejandro Mendez Reyes
+
+*****************/
+
+public class television extends Electrodomestico{
+    private int resolucion;
+    private boolean sintonizadorTDT;
+    private final static int RESOLUCION_DEF=20;
+
+    public television(){
+       this(PRECIO_BASE_DEF, PESO_DEF, CONSUMO_ENERGETICO_DEF, COLOR_DEF, RESOLUCION_DEF, false);
+   }
+   public television(double precioBase, double peso){
+        this(precioBase, peso, CONSUMO_ENERGETICO_DEF, COLOR_DEF, RESOLUCION_DEF, false);
+    }
+    public television(double precioBase, double peso, char consumoEnergetico, String color, int resolucion, boolean sintonizadorTDT){
+       super(precioBase, peso, consumoEnergetico, color);
+       this.resolucion=resolucion;
+       this.sintonizadorTDT=sintonizadorTDT;
+   }
+   public double precioFinal(){
+       double plus=super.precioFinal();
+       if (resolucion>40){
+           plus+=precioBase*0.3;
+       }
+       if (sintonizadorTDT){
+           plus+=50;
+       }
+       return plus;
+   }
+}
